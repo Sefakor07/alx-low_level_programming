714971d7126ed2158b521f10e30424ec086753bc
sefakor07 <annelizay6@gmail.com>
2023-05-04 07:55:13 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
  * main - add one line
@@ -23,6 +22,6 @@ p = &n;
    */
 *(p + 5) = 98;
   /* ...so that this prints 98\n */
-printf("a[2] = %d\n", a[2]);
+printf("a[2] = %d\n" a[2]);
 return (0);
 }
